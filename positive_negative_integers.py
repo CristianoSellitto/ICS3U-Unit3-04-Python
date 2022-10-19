ec9360cd0ee66d46e461a52413610a34548079ee
Cristiano Sellitto <cristiano.sellitto@mths.ca>
2022-10-19 14:55:43 +0000

    Added program

---
+++ positive_negative_integers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created by Cristiano Sellitto
+# Created in October 2022
+# A program that finds if the user's integer is positive, negative, or zero
+
+
+def main():
+    # Finds if the user's integer is positive, negative, or zero
+
+    integer = int(input("Enter a number from 0-9: "))
+    if integer > 0:
+        print("\n{} is a positive integer.".format(integer))
+    elif integer < 0:
+        print("\n{} is a negative integer.".format(integer))
+    else:
+        print("\n{} is zero.".format(integer))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
